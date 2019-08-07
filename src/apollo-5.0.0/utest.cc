12d2901b8a6ab2d70a341681ef8dc35ce14d05ce
yu2 zhang <yu2.zhang@uisee.com>
2019-08-07 18:48:29 +0800

    add gtest case and cmakelists

---
+++ src/apollo-5.0.0/utest.cc
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(apollo_test, util){
+    std::cout << "gtest working." << std::endl;
+}
