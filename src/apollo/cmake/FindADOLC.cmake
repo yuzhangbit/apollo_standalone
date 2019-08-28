3e140c41ca9a7c1dc9f8f65b08fc4343bcb8a047
yuzhangbit <yu2.zhang@uisee.com>
2019-08-28 23:53:49 +0800

    add planning common

---
+++ src/apollo/cmake/FindADOLC.cmake
@@ -0,0 +1,27 @@
+# Find ADOLC
+#
+# This sets the following variables:
+# ADOLC_FOUND
+# ADOLC_INCLUDE_DIRS
+# ADOLC_LIBRARIES
+# ADOLC_DEFINITIONS
+# ADOLC_VERSION
+
+find_path(adolc_INCLUDE_PATH adolc/adolc.h)
+
+find_library(adolc_LIBRARY libadolc.la PATHS /usr/local/lib64)
+find_library(adolc_LIBRARY  libadolc.so PATHS /usr/local/lib64)
+if(adolc_INCLUDE_PATH AND adolc_LIBRARY)
+  set(ADOLC_FOUND TRUE)
+endif(adolc_INCLUDE_PATH AND adolc_LIBRARY)
+
+if(ADOLC_FOUND)
+  message(STATUS "Found adolc:" ${adolc_LIBRARY})
+  # Output variables
+  set(ADOLC_INCLUDE_DIRS ${adolc_INCLUDE_PATH})
+  set(ADOLC_LIBRARIES ${adolc_LIBRARY})
+else(ADOLC_FOUND)
+  if(ADOLC_FIND_REQUIRED)
+    message(FATAL_ERROR "Could not find adolc library.")
+  endif(ADOLC_FIND_REQUIRED)
+endif(ADOLC_FOUND)
---
+++ src/apollo/cmake/FindIPOPT.cmake
@@ -0,0 +1,28 @@
+# Find IPOPT
+#
+# This sets the following variables:
+# IPOPT_FOUND
+# IPOPT_INCLUDE_DIRS
+# IPOPT_LIBRARIES
+# IPOPT_DEFINITIONS
+# IPOPT_VERSION
+
+find_package(PkgConfig)
+
+pkg_check_modules(PC_IPOPT REQUIRED ipopt>=3.12.4)
+
+set(IPOPT_DEFINITIONS ${PC_IPOPT_CFLAGS_OTHER})
+set(IPOPT_INCLUDE_DIRS ${PC_IPOPT_INCLUDE_DIRS})
+set(IPOPT_LIBRARIES ${PC_IPOPT_LIBRARIES})
+set(IPOPT_VERSION ${PC_IPOPT_VERSION})
+include(FindPackageHandleStandardArgs)
+# if all listed variables are TRUE
+find_package_handle_standard_args(IPOPT DEFAULT_MSG
+  IPOPT_LIBRARIES IPOPT_INCLUDE_DIRS)
+mark_as_advanced(IPOPT_INCLUDE_DIRS IPOPT_LIBRARIES)
+
+if(${IPOPT_FOUND})
+  message(STATUS "Found IPOPT version: " ${IPOPT_VERSION} " installed in: " ${PC_IPOPT_PREFIX})
+else()
+  message(SEND_ERROR "Could not find IPOPT")
+endif()
