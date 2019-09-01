dc307ca58fbc16715b62846e288494e40b24f50c
yuzhangbit <yu2.zhang@uisee.com>
2019-09-01 11:41:56 +0800

    fix ipopt install script

--- scripts/install_ipopt.sh
+++ scripts/install_ipopt.sh
@@ -4,14 +4,11 @@ SOURCE_DIR="/tmp"
 
 install_ipopt()
 {
-    #sudo add-apt-repository universe && apt-get -y update
-    #sudo apt-get install -y libblas-dev liblapack-dev gfortran
-    cd /tmp
-    wget https://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.11.zip -O Ipopt-3.12.11.zip
-    unzip Ipopt-3.12.11.zip
-    cd Ipopt-3.12.11/ThirdParty/Mumps
-    bash get.Mumps
-    cd /tmp/Ipopt-3.12.11
+    sudo add-apt-repository universe && apt-get -y update
+    sudo apt-get install -y libblas-dev liblapack-dev gfortran
+    cd ${SOURCE_DIR} && rm -r Ipopt
+    git clone --recursive https://github.com/coin-or/Ipopt.git
+    cd Ipopt && git checkout releases/3.12.11
     ./configure --prefix /usr/local --disable-shared ADD_CXXFLAGS="-fPIC" ADD_CFLAGS="-fPIC" ADD_FFLAGS="-fPIC"
     make -j8 all
     sudo make install
