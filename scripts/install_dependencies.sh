c545d266ca5dd078395b70dbca77021272185678
yuzhangbit <yu2.zhang@uisee.com>
2019-09-01 23:23:08 +0800

    update script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,12 +2,12 @@
 set -e  # exit on first error
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-bash ${CURRENT_DIR}/install_curlpp.sh
-bash ${CURRENT_DIR}/install_colpack_adolc.sh
-bash ${CURRENT_DIR}/install_glogs_gflags.sh
-bash ${CURRENT_DIR}/install_ipopt.sh
-bash ${CURRENT_DIR}/install_osqp.sh
-bash ${CURRENT_DIR}/install_proj4.sh
-bash ${CURRENT_DIR}/install_protobuf.sh
+#bash ${CURRENT_DIR}/install_curlpp.sh
+#bash ${CURRENT_DIR}/install_colpack_adolc.sh
+#bash ${CURRENT_DIR}/install_glogs_gflags.sh
+#bash ${CURRENT_DIR}/install_ipopt.sh
+#bash ${CURRENT_DIR}/install_osqp.sh
+#bash ${CURRENT_DIR}/install_proj4.sh
+#bash ${CURRENT_DIR}/install_protobuf.sh
 bash ${CURRENT_DIR}/install_qpOASES.sh
 bash ${CURRENT_DIR}/install_tinyxml2.sh
--- scripts/install_qpOASES.sh
+++ scripts/install_qpOASES.sh
@@ -3,7 +3,7 @@ set -e  # exit on first error
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 UBUNTU_CODENAME=$(lsb_release -sc)
 
-cd /tmp  && rm -r qpOASES && git clone https://github.com/robotology-dependencies/qpOASES.git
+cd /tmp  && rm -rf qpOASES && git clone https://github.com/robotology-dependencies/qpOASES.git
 cd qpOASES
 mkdir -p build
 cd build
