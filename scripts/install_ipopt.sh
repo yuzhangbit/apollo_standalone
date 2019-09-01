9d35504d66c1a21a48278df8121f73f4fc897994
yuzhangbit <yu2.zhang@uisee.com>
2019-09-01 22:57:27 +0800

    update ipopt script

--- scripts/install_ipopt.sh
+++ scripts/install_ipopt.sh
@@ -6,7 +6,7 @@ install_ipopt()
 {
     sudo add-apt-repository universe &&  sudo apt-get -y update
     sudo apt-get install -y libblas-dev liblapack-dev gfortran
-    cd ${SOURCE_DIR} && rm -r Ipopt
+    cd ${SOURCE_DIR} && rm -rf Ipopt
     git clone --recursive https://github.com/coin-or/Ipopt.git
     cd Ipopt && git checkout releases/3.12.11
     ./configure --prefix /usr/local --disable-shared ADD_CXXFLAGS="-fPIC" ADD_CFLAGS="-fPIC" ADD_FFLAGS="-fPIC"
