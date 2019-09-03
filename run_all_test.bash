d527f318ec57b1dafb5953f685c1ca45d9409e88
ytiang <1044338070@qq.com>
2019-09-03 15:41:21 +0800

    test script

--- run_all_test.bash
+++ run_all_test.bash
@@ -1,13 +1,19 @@
 #! /bin/bash
+set -e
 function run_all_test() {
     cd /apollo/bin
     declare -i file_count
     file_count=0
+    skip_file="file_test"
     for file in `ls`
     do 
         echo -e "\033[43;35m >>>>>>>> testint file: $file \033[0m"
-        ./$file
-        file_count=$file_count+1
+        if [ $file = $skip_file ]; then
+            echo 'skip file_test.....'
+        else
+            ./$file
+            file_count=$file_count+1
+        fi
     done
     echo "Test $file_count files Over"
 }
